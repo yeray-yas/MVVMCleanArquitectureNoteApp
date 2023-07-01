31b4675350b58e24b4da076fe10a2cc98e7e83eb
yeray-yas <yerayarucas@gmail.com>
2023-07-02 00:43:47 +0100

    NoteUseCases

---
+++ app/src/main/java/com/yerayyas/mvvmcleanarquitecturenoteapp/feature_note/domain/use_case/NoteUseCases.kt
@@ -0,0 +1,8 @@
+package com.yerayyas.mvvmcleanarquitecturenoteapp.feature_note.domain.use_case
+
+data class NoteUseCases (
+    val getNotes:GetNotesUseCase,
+    val deleteNote: DeleteNoteUseCase,
+    //val addNoteUseCase:AddNoteUseCase,
+    //val getNoteUseCase:GetNoteUseCase
+)
